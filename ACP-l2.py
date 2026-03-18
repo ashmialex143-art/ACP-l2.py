fe2ac4524ae9bf0d70f4887ea78bcae78634802b
ashmialex143-art <ashmialex143@gmail.com>
2026-03-18 16:36:40 +0530

    Add files via upload

---
+++ ACP-l2.py
@@ -0,0 +1,17 @@
+print("hi,this is ashmi.\n")
+print("Birthdays of 5 friends are:\n")
+
+A="18th august"
+print("A's birthday is on -",A)
+
+B="7th february"
+print("B's birthday is on -",B)
+
+C="25th october"
+print("C's birthday is on -",C)
+
+D="30th may"
+print("D's birthday is on -",D)
+
+E="10th march"
+print("E's birthday is on -",E)
